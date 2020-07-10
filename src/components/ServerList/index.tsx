bfbf8b06de93be742f5d829db12f8a21ad982dae
Miguel <miguelsousalima@hotmail.com>
2020-07-09 23:49:30 -0300

    Comeco Clone Discord

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Container, Separator } from './styles';
+import ServerButton from '../ServerButton'
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome/>
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={4} />
+      <ServerButton />
+      <ServerButton mentions={4} />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={4} />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton />
+    </Container>
+  )
+}
+
+export default ServerList; 
