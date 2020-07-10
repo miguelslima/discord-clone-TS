c6e4bbf86dc02a8235f80f5e8a228845a1e740c2
Miguel <miguelsousalima@hotmail.com>
2020-07-10 09:47:35 -0300

    UserInfo e UserList

---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { Container, Role, User, Avatar } from './styles'; 
+
+interface UserProps {
+  nickname: string;
+  isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({ nickname, isBot }) => {
+  return (
+    <User>
+      <Avatar className={isBot ? 'bot' : ''} />
+
+      <strong>{nickname}</strong>
+    
+      {isBot && <span>BOT</span>}
+    </User>
+  )
+}
+
+
+const UserList: React.FC = () => {
+  return (
+    <Container>
+      <Role>Disponível - 1</Role>
+      <UserRow nickname="Gnomo Louco" />
+
+      <Role>Offline - 18</Role>
+      <UserRow nickname="Diego Fernandes" isBot />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+      <UserRow nickname="Fulano de tal"  />
+    </Container>
+  )
+}
+
+export default UserList;    
